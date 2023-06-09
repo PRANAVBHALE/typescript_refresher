9686a9acee8a814c545eba6ea21d994e412c1311
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-09 23:21:22 +0530

    annotations for variables

---
+++ annotations/variables.ts
@@ -0,0 +1,28 @@
+let mangoes: number = 20;
+
+//works
+mangoes = 10;
+
+//dont work
+//mangoes = 'mangoes';
+
+let mystring: string = 'string';
+
+//works
+mystring = 'works';
+
+//dont work
+//mystring = 10;
+
+let isBoolean: boolean = true;
+
+//works
+isBoolean = false;
+
+//dont work
+//isBoolean = 'false';
+
+let today: Date = new Date();
+
+// dont work
+//today = "today"
