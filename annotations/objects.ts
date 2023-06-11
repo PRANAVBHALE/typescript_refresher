b0b475843aba8e795ff97e4cb73eb282b3fb092d
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-11 18:29:03 +0530

    annotations in destructing of objects

--- annotations/objects.ts
+++ annotations/objects.ts
@@ -5,16 +5,6 @@ let cars: string[] = ['i20', 's-cross', 'venue'];
 
 let mynumbers: number[] = [1, 2, 3];
 
-class Animal {
-  legs: '4';
-}
-
-let dog: Animal = new Animal();
-console.log(dog.legs);
-
-//dont work
-//console.log(dog.hands);
-
 let rectriangle: { length: number; breadth: number; name: string } = {
   length: 10,
   breadth: 20,
@@ -22,3 +12,28 @@ let rectriangle: { length: number; breadth: number; name: string } = {
   //dont work
   //height:10
 };
+
+const profile = {
+  firstname: 'pranav',
+  age: 30,
+
+  location: {
+    lat: 21.4,
+    long: 15.1321,
+  },
+};
+
+//destructing
+
+const { firstname }: { firstname: string } = profile;
+
+const {
+  location: { lat, long },
+}: { location: { lat: number; long: number } } = profile;
+
+//steps to do
+//1. Destructure normally as in ES6
+//2. Add : to add types
+//3. Mirror the destructure and add types
+
+console.log(lat, long);
