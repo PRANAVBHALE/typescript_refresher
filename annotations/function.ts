66dae805555eaa819c4580d01fc84853fec96027
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-11 16:53:03 +0530

    destructing and using annotation

--- annotations/function.ts
+++ annotations/function.ts
@@ -31,3 +31,21 @@ const logger = (message: string): void => {
 };
 
 logger('Hello TS');
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
