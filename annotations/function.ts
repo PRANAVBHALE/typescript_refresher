969b48c49b70370b60514c9f4feeebe1c821a5e3
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-11 16:17:35 +0530

    annotation for function itself

--- annotations/function.ts
+++ annotations/function.ts
@@ -1,7 +1,14 @@
+//annotation for function variable
 const myFn: (i: number) => number = (i) => {
-  console.log(i);
-
   return i;
 };
 
-myFn(5);
+console.log(myFn(5));
+
+//annotation for function itself
+
+const myFn2 = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(myFn2(10, 20));
