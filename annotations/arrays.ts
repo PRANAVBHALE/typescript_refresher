2e243cf422e4a4fb3cbe3e12665f8c9e1c1f4441
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-11 18:48:28 +0530

    annocations for arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,7 @@
+const movies: string[] = ['avengers', 'Batman', 'Spiderman'];
+
+const moviesByMakers: string[][] = [
+  ['avengers', 'Ironman', 'Spoderman'],
+  ['Batman', 'Wonderwoman', 'Superman'],
+  ['Casino Royale', 'Spectre'],
+];
