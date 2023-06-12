b26ee85fece2211d0153f5b558c0873ecd38ce1e
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-12 21:58:01 +0530

    difference between tuples and objects

--- annotations/tuples.ts
+++ annotations/tuples.ts
@@ -14,3 +14,14 @@ type Drink = [string, boolean, number];
 
 const coke: Drink = ['brown', true, 40];
 const tea: Drink = ['brown', false, 20];
+
+//dont use tuple if similar types are present or there is not enough information is present about data
+//eg
+
+const carSpecs: [number, number] = [400, 4000];
+
+//instead use object for better object modeling
+const carDetails = {
+  maxSpeed: 400,
+  engineCapacity: 4000,
+};
