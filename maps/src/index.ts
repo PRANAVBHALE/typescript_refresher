6703ffdc006f56fc46db166b2a8df3b49172a98f
PRANAVBHALE <bhalepranav25@gmail.com>
2023-06-14 01:19:10 +0530

    added user and company marker on map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,20 +1,16 @@
 /// <reference types="@types/google.maps" />
 
-// import { Company } from './Company';
-// import { User } from './User';
+import { CustomMap } from './CustomMap';
 
-// const company = new Company();
-// const user = new User();
+import { Company } from './Company';
+import { User } from './User';
+
+const company = new Company();
+const user = new User();
 
 // console.log(user);
 // console.log(company);
 
-console.log(google);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
